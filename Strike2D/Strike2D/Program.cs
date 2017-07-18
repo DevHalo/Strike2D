29917f629925ab5e0c3d51d2e24bf8a61f91661e
Mark Voong <devhalo@hotmail.com>
2017-07-18 00:31:14 -0400

    Created MonoGame project from NuGet

---
+++ Strike2D/Strike2D/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strike2D
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Strike2DMain())
+                game.Run();
+        }
+    }
+}
