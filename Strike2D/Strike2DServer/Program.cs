daf4877e0f752b354eeab10e3ae5ac719d75a547
Mark Voong <devhalo@hotmail.com>
2017-07-23 15:53:08 -0400

    Added server project

---
+++ Strike2D/Strike2DServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strike2DServer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
